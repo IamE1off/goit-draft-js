3661c7639af18a5b91ddf423f84f1bc7fe4f6d18
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-01-18 16:26:25 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -8,11 +8,37 @@
 // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-const person = {
-  name: "Mango",
+// const person = {
+//   name: "Mango",
+// };
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+// console.log(sayHi.call(person, 18));
+// console.log(sayHi.bind(person, 18));
+
+// const user = {
+//   username: "Victor",
+//   showName() {
+//     // ❌ Віктор біжить швидко, тому що Віктор намагається зловити поїзд
+//     console.log(this.username);
+//   },
+// };
+
+// user.showName();
+// const book = {
+//   updateAuthor(newAuthor) {
+//     console.log("this in updateAuthor: ", this); // ...
+//   },
+// };
+
+// book.updateAuthor("Jacob");
+const book = {
+  updateTitle(newTitle) {
+    // ...
+  },
 };
-function sayHi(age) {
-  return `${this.name} is ${age}`;
-}
-console.log(sayHi.call(person, 18));
-console.log(sayHi.bind(person, 18));
+
+const update = book.updateTitle;
+
+update("Lord Of The Rings");
