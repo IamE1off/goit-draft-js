be4e6e598271934a0114f2798c0dfda82120bce0
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-01-18 14:35:54 +0100

    ini

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,18 @@
 "use strict";
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
 
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const person = {
+  name: "Mango",
+};
+function sayHi(age) {
+  return `${this.name} is ${age}`;
 }
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(sayHi.call(person, 18));
+console.log(sayHi.bind(person, 18));
